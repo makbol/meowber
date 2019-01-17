344d2ac04aae969fcc422f298a8f1743eead8dda
Max <makbol@gmail.com>
2019-01-18 00:01:44 +0100

    Remove unnecessary args for debounce

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,10 +2,9 @@ export function debounce (fn, delay) {
     let timeoutID = null;
     return function () {
         clearTimeout(timeoutID);
-        let args = arguments;
         let that = this;
         timeoutID = setTimeout(function () {
-            fn.apply(that, args);
+            fn.apply(that);
         }, delay);
     };
 }
